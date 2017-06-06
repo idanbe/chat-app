c8ef690a6818bff7aa4e566596956c78ee001b50
idanbe <idanbeck88@gmail.com>
2017-06-06 11:01:16 +0300

    Add geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,15 @@
 		jQuery('#messages').append(li);
 	});
 
+	socket.on('newLocationMessage', function (message) {
+		var li = jQuery('<li></li>');
+		var a = jQuery('<a target="_blank">My current location</a>')	
+		li.text(`${message.from}: `);
+		a.attr('href',message.url);
+		li.append(a);
+		jQuery('#messages').append(li);
+
+	});
 
 
 
@@ -28,5 +37,21 @@
 			text: jQuery('[name=message]').val()
 		} , function () {
 
+		});
+	});
+
+
+	var locationButton = jQuery('#send-location');
+	locationButton.on('click',function() {
+		if(!navigator.geoloction) {
+			return alert('Geoloction not supported by your browser.');
+		}
+		navigator.geoloction.getCurrentPosition(function(position) {
+			socket.emit('createLocationMessage',{
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			});
+		},function () {
+			alert('Unable to fetch location.');
 		});
 	});
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 
 
@@ -12,5 +12,20 @@ describe('generateMessage', () =>{
 
 		expect(message.createdAt).toBeA('number');
 		expect(message).toInclude({from,text});
+	});	
+});
+
+
+describe('generateLocationMessage', () =>{
+	it('should generate correct location message object' ,() =>{
+		var from = 'idan';
+		var latitide=15;
+		var longitude=19;
+		var url = 'https://www.google.co.il/maps?q=15,19';
+		var message = generateLocationMessage(from,latitide,longitude);
+
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({from,url});
+
 	});	
 });
