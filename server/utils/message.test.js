67fffda45115ae1ede4a22a24eff5b1f22e8b343
idanbe <idanbeck88@gmail.com>
2017-06-15 19:07:31 +0300

    add users class

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,31 +1,27 @@
 var expect = require('expect');
 
-var {generateMessage,generateLocationMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
 
-
-describe('generateMessage', () =>{
-	it('should generate correct message object' ,() =>{
-		var from = 'idan';
-		var text = 'hay';
-		var message = generateMessage(from,text);
-
-		expect(message.createdAt).toBeA('number');
-		expect(message).toInclude({from,text});
-	});	
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
 });
 
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
 
-describe('generateLocationMessage', () =>{
-	it('should generate correct location message object' ,() =>{
-		var from = 'idan';
-		var latitide=15;
-		var longitude=19;
-		var url = 'https://www.google.co.il/maps?q=15,19';
-		var message = generateLocationMessage(from,latitide,longitude);
-
-		expect(message.createdAt).toBeA('number');
-		expect(message).toInclude({from,url});
-
-	});	
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
 });
